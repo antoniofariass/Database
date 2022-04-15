ecabed77e1bf8bc1e4a0b1270381a58783801b55
AntonioFarias <antoniotercerio@gmail.com>
2022-04-14 22:49:54 -0300

    adding file

---
+++ Studies-SQL/29-UPDATE.sql
@@ -0,0 +1,12 @@
+--SERVE PARA ATUALIZAR LINHAS NO BANCO DE DADOS 
+
+UPDATE nomeTabela
+SET coluna1 = valor1
+    coluna2 = valor2
+WHERE condicao  --utilizar where para atualizar linhas especificas 
+
+UPDATE Aula
+SET nome = 'mudei'
+WHERE id = 3
+
+
